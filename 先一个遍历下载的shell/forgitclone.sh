f579faad736f01642ce0ba7191e4c4738fae0488
Greenbirch2007 <291109028@qq.com>
2020-02-03 13:51:09 +0800

    golang等技术全面下架，为ry让路

--- 先一个遍历下载的shell/forgitclone.sh
+++ 先一个遍历下载的shell/forgitclone.sh
@@ -6,7 +6,6 @@ do
   #wget "https://codeload.github.com/golang//zip/master"
 
 
-	git clone https://github.com/golang/$url.git $GOPATH/src/github.com/golang/$url
-
+	git clone https://github.com/golang/$url.git 
 
 done
